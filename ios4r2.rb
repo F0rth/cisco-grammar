43e92af8575b3b3773c0c3b0e0d3078e65b5df66
forth <forth@ela.univ-rennes1.fr>
2010-04-09 11:24:25 +0200

    go go go

---
+++ ios4r2.rb
@@ -0,0 +1,65 @@
+require 'strscan'
+require 'yaml'
+require 'pp'
+require 'rubygems'
+require 'treetop'
+
+class Parser
+
+	attr_accessor :config, :token, :tokens_list, :parsed_hashes
+	
+	
+	def initialize
+	@config = IO.readlines(ARGV[0])
+	@tokens_list = Array.new
+	@parsed_hashes = Array.new
+	
+	end
+	
+	def find_lines
+		token_regexp = Regexp.new(@token)
+		@config.each_index {|index|
+			scan = StringScanner.new(@config[index])
+			if scan.scan(token_regexp) == @token
+				@tokens_list << @config[index]
+				index +=1
+				scan = StringScanner.new(@config[index])
+			end
+		}
+	end
+	
+	def parse_lines
+		dir = File.dirname(__FILE__)
+		
+		if @token.include? " "
+			grammar = @token.gsub(/ /,'_')
+		else
+			grammar = @token
+		end
+
+		Treetop.load dir + "/parser/" + grammar
+		parsername = grammar.capitalize + "Parser"
+		parser = Object.module_eval("::#{parsername}").new
+
+		@tokens_list.each {|line|
+		result = parser.parse(line)
+
+		values = Hash.new
+		result.singleton_methods.each {|method| 
+			begin
+				value = {method => result.send(method).text_value} 
+				values.update(value)
+			end
+		}
+		@parsed_hashes << values
+		}
+	end
+
+end
+
+z = Parser.new
+z.token = "interface"
+z.find_lines
+pp z.tokens_list
+#z.parse_lines
+#pp z.parsed_hashes
