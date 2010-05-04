26d614959780808dc92f433ac2e55b0b8dfbce55
forth <forth@ela.univ-rennes1.fr>
2010-05-04 13:53:37 +0200

    tests

--- routeur.rb
+++ routeur.rb
@@ -36,14 +36,15 @@ class Router
 	
 	def process_packet(packet)
 		# tant que l'on a une adresse ip en sortie, on cherche l'interface
-		node = @routing_table.add(packet.ip_dst)
+		node = @decision_tree.search_best(packet.predicat.ip_dst)
 		begin
 			while node.data.to_ip.is_a? IPAddr
 				packet.predicat.ip_dst = node.data
-				process_packet(packet) 
+				process_packet(packet)
+			end
 		rescue
 			packet.next_hop = node.prefix
-			return node
+			packet.if_out = node.data
 		end
 		
 	end
@@ -67,9 +68,46 @@ class Route
 end
 
 class Packet
-	attr_accessor :predicat, :next_hop
+	attr_accessor :predicat, :next_hop, :if_in, :if_out 
 end
 
 class Predicat
 	attr_accessor :protocol, :ip_src, :prt_dst, :ip_dst, :prt_src
 end
+
+
+predi = Predicat.new
+predi.ip_src = "192.168.1.3/32"
+predi.ip_dst = "192.168.3.4/32"
+
+pac = Packet.new
+pac.predicat = predi
+
+eth0 = Interface.new
+eth1 = Interface.new
+
+eth0.name = "eth0"
+eth0.ip_address = "192.168.3.254/24"
+
+eth1.name = "eth1"
+eth1.ip_address = "192.168.30.254/24"
+
+
+
+route1 = Route.new
+route2 = Route.new
+
+route1.ip_src = "192.168.5.0/24"
+route1.ip_dst = "192.168.3.18"
+
+route2.ip_src = "192.168.6.0/24"
+route2.ip_dst = "192.168.7.34"
+
+rou = Router.new([eth0, eth1], [route1, route2])
+
+
+# rou.decision_tree.show_nodes
+
+rou.process_packet(pac)
+p pac.if_out
+p pac.next_hop
