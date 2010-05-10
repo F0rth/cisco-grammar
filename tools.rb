97c6648d69b309658d879e63d97d95788747a1bd
forth <forth@ela.univ-rennes1.fr>
2010-05-10 09:21:25 +0200

    commit

--- tools.rb
+++ tools.rb
@@ -1,9 +1,11 @@
-module bits_helper
+require 'ipaddr'
+
+module Tools
 # http://groups.google.com/group/comp.lang.ruby/browse_thread/thread/a8e4e2e860db9c45
 # transforme un entier en tableau de bits
-class Integer 
+module IntegerExtensions
 	def to_ba(size=8) 
-    	a=[] 
+		a=[]
 		(size-1).downto(0) do |i| 
 		a<<self[i] 
 		end 
@@ -13,9 +15,26 @@ end
 
 # transforme un tableau de bits en entier
 
-class Array
+module ArrayExtensions
 	def to_i(radix=2)
 		self.to_s.to_i(radix)
 	end
 end
-	
+
+module RangeExtensions
+	def intersec?(range)
+		self.to_a & range.to_a
+	end
+end
+
+module IPAddrExtensions
+	alias intersec? |
+end
+
+end
+
+
+Integer.send(:include, Tools::IntegerExtensions)
+Array.send(:include, Tools::ArrayExtensions)
+Range.send(:include, Tools::RangeExtensions)
+IPAddr.send(:include, Tools::IPAddrExtensions)
