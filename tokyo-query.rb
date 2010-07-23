1fa0307000fceae18be11d1137cc1540f09fadc3
forth <forth@ela.univ-rennes1.fr>
2010-07-23 10:23:24 +0200

    grammaire pix

--- tokyo-query.rb
+++ tokyo-query.rb
@@ -1,10 +1,10 @@
 require 'storage'
-
+require 'pp'
 
 s = Storage.new('ios.tct')
 
   pp s.db.query { |q|
    q.add_condition 'type', :equals, 'acl'
-    q.order_by 'destination_ip'
+    q.order_by 'index'
 }
 s.db.close
