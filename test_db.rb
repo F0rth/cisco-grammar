bb79e1445b5c3020608f6ab1c3e42ee7466d6c0b
F0rth <forth@ophane.net>
2010-08-01 13:37:25 +0200

    raffinement regle log de la grammaire pix

--- test_db.rb
+++ test_db.rb
@@ -9,7 +9,16 @@ include Translator
 s_pix = Storage.new('pix.tct')
 
 pix_names = s_pix.db.query { |q|
-			q.add_condition 'index', :equals, '2024'
+			q.add_condition 'destination_port', :strinc, 'log'
+			q.order_by 'index'
+			}
+
+
+pp pix_names
+
+
+pix_names = s_pix.db.query { |q|
+			q.add_condition 'index', :equals, '2011'
 			q.order_by 'index'
 			}
 
